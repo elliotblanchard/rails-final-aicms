cf2e44d75f179cfabcc04d4d7a04967452df7127
Elliot Blanchard <git@elliotblanchard.com>
2020-08-21 22:15:24 -0400

    connected api to live data

--- app/models/generator.rb
+++ app/models/generator.rb
@@ -10,6 +10,24 @@ class Generator < ApplicationRecord
     validates :description, presence: true        
 
     def generator_request(params)
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        authorization = "Bearer #{ENV['INFERKIT_KEY']}"
+        start_from_beginning = params[:start_at_beginning].to_i == 1 ? (true) : (false)
+        force_no_end = params[:pause_at_end].to_i == 1 ? (true) : (false)
+        is_continuation = params[:is_continuation].to_i == 1 ? (true) : (false)
+
+        response = HTTParty.post(self.api_url, 
+        :body => { :prompt => { :text => params[:prompt], :isContinuation => is_continuation }, 
+                :length => params[:length].to_i,
+                :startFromBeginning => start_from_beginning,
+                :streamResponse => false,
+                :forceNoEnd => force_no_end,
+                :topP => params[:top_p].to_f,
+                :temperature => params[:temperature].to_f
+                }.to_json,
+        :headers => { :authorization => authorization } ) 
+        parsed = JSON.parse(response.body)
+        parsed["data"]["text"]
+        
+        #"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     end
 end
